3cade9bf66a63811bff65ff73429327a0fbf867d
EBarette <edouard.barette@gmail.com>
2024-03-31 18:28:52 +0200

    small fixes

--- domain_name.py
+++ domain_name.py
@@ -1,4 +1,6 @@
 import pyshark
+import nest_asyncio
+nest_asyncio.apply()
 
 def process_domain_name(filename):
     cap = pyshark.FileCapture(filename)
@@ -16,6 +18,35 @@ if __name__ == "__main__":
     print("imgToDrive.pcapng")
     for pkt in process_domain_name("WiresharkCapture/imgToDrive.pcapng"):
         print(pkt)
+    
     print("imgToDrive2.pcapng")
     for pkt in process_domain_name("WiresharkCapture/imgToDrive2.pcapng"):
+        print(pkt)
+    
+    print("imgToDrive3.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/imgToDrive3.pcapng"):
+        print(pkt)
+    
+    print("imgToDrive4.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/imgToDrive4.pcapng"):
+        print(pkt)
+    
+    print("imgToDrive5.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/imgToDrive5.pcapng"):
+        print(pkt)
+    
+    print("Linux_Wifi_pdfTo&OffDrive.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/Linux_Wifi_pdfTo&OffDrive.pcapng"):
+        print(pkt)
+    
+    print("Linux_Wifi_pdfToDrive.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/Linux_Wifi_pdfToDrive.pcapng"):
+        print(pkt)
+    
+    print("Linux_Wifi_pdfToDrive2.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/Linux_Wifi_pdfToDrive2.pcapng"):
+        print(pkt)
+    
+    print("Linux_Wifi_pdfToDrive3.pcapng")
+    for pkt in process_domain_name("WiresharkCapture/Linux_Wifi_pdfToDrive3.pcapng"):
         print(pkt)
