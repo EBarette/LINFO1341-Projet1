0acbc2a03d96389eda7ee993eb782a4121884d61
EBarette <edouard.barette@gmail.com>
2024-04-02 16:43:58 +0200

    modif data_flow

--- data_flow.py
+++ data_flow.py
@@ -22,66 +22,82 @@ def process_data_flow(filename):
 
 def pretty_print(filename):
     print(filename)
-    print("Data flow: " + str(process_data_flow(filename)) + "\n")
+    data = process_data_flow(filename)
+    print("Data flow: " + str(data) + "\n")
+    return data
+
 
 if __name__ == "__main__":
 
     # AJOUT DE FICHIER PCAPNG
+    moyenne = 0
     
-    pretty_print("AjoutDeFichiers/imgToDrive.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/imgToDrive.pcapng")
     
-    pretty_print("AjoutDeFichiers/imgToDrive2.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/imgToDrive2.pcapng")
     
-    pretty_print("AjoutDeFichiers/imgToDrive3.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/imgToDrive3.pcapng")
     
-    pretty_print("AjoutDeFichiers/imgToDrive4.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/imgToDrive4.pcapng")
     
-    pretty_print("AjoutDeFichiers/imgToDrive5.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/imgToDrive5.pcapng")
     
-    pretty_print("AjoutDeFichiers/Linux_WifiUCL_pdfToDrive.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/Linux_WifiUCL_pdfToDrive.pcapng")
     
-    pretty_print("AjoutDeFichiers/Linux_WifiUCL_pdfToDrive2.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/Linux_WifiUCL_pdfToDrive2.pcapng")
     
-    pretty_print("AjoutDeFichiers/Linux_Wifi_pdfToDrive3.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/Linux_Wifi_pdfToDrive3.pcapng")
 
-    pretty_print("AjoutDeFichiers/multipleFilesToDrive.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/multipleFilesToDrive.pcapng")
     
-    pretty_print("AjoutDeFichiers/multipleFilesToDrive2.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/multipleFilesToDrive2.pcapng")
     
-    pretty_print("AjoutDeFichiers/multipleFilesToDrive3.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/multipleFilesToDrive3.pcapng")
 
-    pretty_print("AjoutDeFichiers/Linux_4G_ImageToDrive.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/Linux_4G_ImageToDrive.pcapng")
 
-    pretty_print("AjoutDeFichiers/Linux_4G_PdfToDrive.pcapng")
+    moyenne += pretty_print("AjoutDeFichiers/Linux_4G_PdfToDrive.pcapng")
+
+    print("Moyenne AjoutDeFichiers: " + str(moyenne/13) + "\n")
+    moyenne = 0
 
     # CAS PARTICULIERS
     
-    pretty_print("CasParticuliers/deleteFromPhone.pcapng")
+    moyenne += pretty_print("CasParticuliers/deleteFromPhone.pcapng")
     
-    pretty_print("CasParticuliers/Linux_Wifi_pdfTo&OffDrive.pcapng")
+    moyenne += pretty_print("CasParticuliers/Linux_Wifi_pdfTo&OffDrive.pcapng")
     
-    pretty_print("CasParticuliers/modifyFromPhone.pcapng")
+    moyenne += pretty_print("CasParticuliers/modifyFromPhone.pcapng")
 
-    pretty_print("CasParticuliers/Linux_4G_PdfTo&OffDrive.pcapng")
+    moyenne += pretty_print("CasParticuliers/Linux_4G_PdfTo&OffDrive.pcapng")
+
+    print("Moyenne CasParticuliers: " + str(moyenne/4) + "\n")
+    moyenne = 0
 
     # DELETE DE FICHIERS PCAPNG
     
-    pretty_print("DeleteDeFichiers/deleteMultipleFiles.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/deleteMultipleFiles.pcapng")
     
-    pretty_print("DeleteDeFichiers/deleteMultipleFiles2.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/deleteMultipleFiles2.pcapng")
     
-    pretty_print("DeleteDeFichiers/Phone_4G_ImageOffDrive.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/Phone_4G_ImageOffDrive.pcapng")
 
-    pretty_print("DeleteDeFichiers/Linux_Wifi_pdfOffDrive1.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/Linux_Wifi_pdfOffDrive1.pcapng")
 
-    pretty_print("DeleteDeFichiers/Linux_4G_pdfOffDrive.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/Linux_4G_pdfOffDrive.pcapng")
 
-    pretty_print("DeleteDeFichiers/Linux_4G_ImageOffDrive.pcapng")
+    moyenne += pretty_print("DeleteDeFichiers/Linux_4G_ImageOffDrive.pcapng")
+
+    print("Moyenne DeleteDeFichiers: " + str(moyenne/6) + "\n")
+    moyenne = 0
 
     # MODIF DE FICHIERS PCAPNG
     
-    pretty_print("ModifDeFichiers/modifyFileOnDrive.pcapng")
+    moyenne += pretty_print("ModifDeFichiers/modifyFileOnDrive.pcapng")
     
-    pretty_print("ModifDeFichiers/modifyFileOnDrive2.pcapng")
+    moyenne += pretty_print("ModifDeFichiers/modifyFileOnDrive2.pcapng")
     
-    pretty_print("ModifDeFichiers/modifyFileOnDrive3.pcapng")
+    moyenne += pretty_print("ModifDeFichiers/modifyFileOnDrive3.pcapng")
+
+    print("Moyenne ModifDeFichiers: " + str(moyenne/3) + "\n")
+    moyenne = 0
